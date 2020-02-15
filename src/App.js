b09cf62ba37801f1c731d6b1d69ba166a4b373e6
DzianisEmcev <edenis@tut.by>
2020-02-15 16:41:50 +0300

    EDU-3.3 fix EDU-3.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar navbar-light bg-light justify-content-between">
+        <form className="form-inline">
+          <button type="button" className="btn btn-primary btn-sm">Log In</button>
+          <button type="button" className="btn btn-secondary btn-sm">Sign Up</button>
+        </form>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
