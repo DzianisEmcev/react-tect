fc0a1324124e1dfc04077d1aa3be67fd72c6e003
DzianisEmcev <edenis@tut.by>
2020-03-02 00:01:28 +0300

    EDU-6 add router

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Connect to `github.com` -- `master` branch
 Use `git clone` with link **https://github.com/DzianisEmcev/react-tect.git**
 
 Start project `npm start`
+
+React router `npm install react-router-dom`
--- src/App.js
+++ src/App.js
@@ -3,16 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import {ButtonComponent} from "./Components/btnComponent";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <nav className="navbar navbar-light bg-light justify-content-between">
-                <form className="form-inline">
-                    <ButtonComponent style="btn btn-primary btn-sm" name="Log In"/>
-                    <ButtonComponent style="btn btn-secondary btn-sm" name="Sign Up"/>
-                </form>
-            </nav>
+            <Router>
+                <div>
+                    <nav className="navbar navbar-light bg-light justify-content-between">
+                        <form className="form-inline">
+                            <ButtonComponent style="btn btn-primary btn-sm" name="Log In"/>
+                            <ButtonComponent style="btn btn-secondary btn-sm" name="Sign Up"/>
+                        </form>
+                    </nav>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/log_in">Log In</Link>
+                                <ButtonComponent style="btn btn-primary btn-sm" name="Log In"/>
+                            </li>
+                            <li>
+                                <Link to="/sign_up">Sign Up</Link>
+                                <ButtonComponent style="btn btn-secondary btn-sm" name="Sign Up"/>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path="/log_in">
+                            <Log_In/>
+                        </Route>
+                        <Route path="/sign_up">
+                            <Sign_Up/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
@@ -29,6 +59,40 @@ function App() {
             </header>
         </div>
     );
+}
+
+function Log_In() {
+    return<div><h2>Log In</h2>
+        <form>
+            <div className="form-group">
+                <label>Name</label>
+                <input type="text" className="form-control" placeholder="Name"/>
+            </div>
+            <div className="form-group">
+                <label>label>Password</label>
+                <input type="password" className="form-control" placeholder="Password"/>
+            </div>
+        </form>
+    </div>;
+}
+
+function Sign_Up() {
+    return <div><h2>Sign Up</h2>
+        <form>
+            <div className="form-group">
+                <label>Name</label>
+                <input type="text" className="form-control" placeholder="Name"/>
+            </div>
+            <div className="form-group">
+                <label>label>Password</label>
+                <input type="password" className="form-control" placeholder="Password"/>
+            </div>
+            <div className="form-group">
+                <label>label>Repeat password</label>
+                <input type="password" className="form-control" placeholder="Repeat password"/>
+            </div>
+        </form>
+    </div>;
 }
 
 export default App;
